87bf2c68b0b6826dabac78f68e9f1b264d231f90
Drago1068 <ivaninazario68@gmail.com>
2026-09-22 10:28:23 -0400

    Propagate separate qualification scores

--- src/tradegpt/qualification.py
+++ src/tradegpt/qualification.py
@@ -74,6 +74,8 @@ class QualificationService:
             stop_price=request.stop_price,
             target_price=request.target_price,
             trigger_confirmed=request.trigger_confirmed,
+            underlying_score=request.underlying_score,
+            execution_score=request.execution_score,
         )
         return self.orchestrator.process(
             scan,
